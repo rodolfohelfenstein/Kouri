70ccc5f70635705afd9ccc7edf4c554d3bd42890
rodolfoh_cit <rodolfoh@ciandt.com>
2015-10-06 22:11:51 -0300

    First Commit Project TCC

---
+++ Assets/Scripts/GameController/PlatformController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformController : MonoBehaviour {
+
+	[HideInInspector] public int Speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x + 2 - transform.parent.position.x < -30)
+			Destroy (gameObject);
+	}
+	
+	void SetSpeed (int pSpeed) {
+		Speed = pSpeed;
+	}
+
+	void FixedUpdate (){
+		transform.Translate (new Vector3 (-Time.fixedDeltaTime * Speed, 0, 0));
+	}
+
+	void Move () {
+
+	}
+}
---
+++ Assets/Scripts/GameController/SpawnerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnerController : MonoBehaviour {
+
+	public int Speed;
+	private int lastSpeed;
+
+	private float nextActionTime = 0.0f;
+
+	private float delayInstantiate = 0.1f;  
+	
+	public List<GameObject> SpawnObjects;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Transform lastChild = transform.GetChild (transform.childCount - 1);
+
+		if (lastChild.position.x + 2 - transform.position.x  < 0 ) {
+			Object NewObject = Instantiate (SpawnObjects [0], lastChild.position + new Vector3(2 - delayInstantiate, -lastChild.position.y + Random.Range(0,3) -2, 0), transform.rotation);
+			((GameObject)NewObject).transform.parent = transform;
+			((GameObject)NewObject).transform.SendMessage ("SetSpeed", Speed);
+			
+		}
+	}
+
+
+	void FixedUpdate () {
+
+				foreach (Transform child in transform)
+				{
+					child.SendMessage("SetSpeed", Speed);
+				}
+
+	}	
+}
